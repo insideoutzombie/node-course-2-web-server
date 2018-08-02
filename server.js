f4806ca5e66a8cf1e5fb47ce2b97ea55ed2389a5
insideoutzombie <david@web3devs.com>
2018-08-02 16:15:11 -0500

    adjusted projects variables

--- server.js
+++ server.js
@@ -50,9 +50,9 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/project', (req, res) => {
-  res.render('project.hbs', {
-    pageTitle: 'Project Page',
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
     currentYear: new Date().getFullYear()
   });
 });
